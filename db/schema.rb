dbddbfc64fa1857ec1fb9bc8b883149b66bebf2d
Ryo-exid <sakura4oneok@gmail.com>
2021-06-04 15:41:40 +0000

    [Fix]新規登録

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_130920) do
+ActiveRecord::Schema.define(version: 2021_06_04_153312) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
@@ -36,9 +36,9 @@ ActiveRecord::Schema.define(version: 2021_06_03_130920) do
     t.string "email"
     t.string "encrypted_password"
     t.integer "postcode"
-    t.integer "prefecture_code"
     t.string "profile_image_id"
     t.text "introduction"
+    t.integer "prefecture_code"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
