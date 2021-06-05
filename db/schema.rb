064c11e94bc1305470da7c780e16732cdeabe2a9
Ryo-exid <sakura4oneok@gmail.com>
2021-06-05 09:24:14 +0000

    [Fix]migration実行した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_081425) do
+ActiveRecord::Schema.define(version: 2021_06_03_115522) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.string "post_image_id"
+    t.text "caption"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
