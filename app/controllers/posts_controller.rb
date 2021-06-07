87d5bbc528b884cd2de729c4bd750a54264283e1
Ryo-exid <sakura4oneok@gmail.com>
2021-06-07 05:40:13 +0000

    [Add]投稿削除機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :update, :destroy]
-  
+
   def new
     @post = Post.new
   end
@@ -23,6 +23,8 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
@@ -30,7 +32,7 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :post_image, :caption, :location)
   end
-  
+
   def find_post
     @post = Post.find(params[:id])
   end
