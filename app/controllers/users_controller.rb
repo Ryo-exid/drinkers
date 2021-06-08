e9d819d484b2460f9d66d86c5dff376f7d037b0e
Ryo-exid <sakura4oneok@gmail.com>
2021-06-08 12:45:57 +0000

    [Add]ユーザ編集ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: [:show, :edit, :update, :destroy]
+  before_action :find_user, only: [:show, :edit, :update]
 
   def index
   end
@@ -11,12 +11,14 @@ class UsersController < ApplicationController
   end
 
   def update
-  end
-
-  def destroy
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
   private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
 
   def find_user
     @user = User.find(params[:id])
