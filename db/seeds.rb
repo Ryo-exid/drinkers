be6c231bba91c372d32a7d87f0ab4f2a275cad4d
Ryo-exid <sakura4oneok@gmail.com>
2021-06-08 08:43:06 +0000

    [Fix]初期データの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,72 +1,72 @@
-# ユーザサンプル
-4.times do |n|
+3.times do |n|
   User.create!(
-    name: "user#{n + 1}",
-    postcode: 111111,
-    address_street: "1-1-1",
-    address_building: "building#{n + 1}",
+    name: "ユーザ#{n + 1}",
+    birth_date: "1996-1-1",
     email: "test#{n + 1}@test.com",
-    password: 111111
+    postcode: 111111,
+    address_city: "台東区",
+    address_street: "1-1-1",
+    address_building: "111",
+    password: "111111"
   )
-end
-
-# 投稿サンプル
-3.times do |n|
-  n = n + 1
-  Post = post.new(
-    user_id: 1,
-    title: "珍しいリキュール#{n}",
-    caption: "Godivaのリキュールを試してみました#{n}",
-    location: "渋谷",
-  )
-  File.open("./app/assets/images/alcohol1.jpg") do |file|
-    post.image = file
-  end
-  post.post_image_id = post.image.id
-  post.save
-end
-
-4.times do |n|
-  n = n + 1
-  Post = post.new(
-    user_id: 2,
-    title: "最高級焼酎飲みました#{n}",
-    caption: "魔王！！絶品です！！#{n}",
-    location: "新宿",
-  )
-  File.open("./app/assets/images/alcohol2.jpg") do |file|
-    post.image = file
-  end
-  post.post_image_id = post.image.id
-  post.save
-end
-
-3.times do |n|
-  n = n + 1
-  Post = post.new(
-    user_id: 3,
-    title: "十四代#{n}",
-    caption: "一杯1500円くらいしますがとっても美味しいです。#{n}",
-    location: "",
-  )
-  File.open("./app/assets/images/alcohol3.jpg") do |file|
-    post.image = file
-  end
-  post.post_image_id = post.image.id
-  post.save
 end
 
 2.times do |n|
-  n = n + 1
-  Post = post.new(
-    user_id: 4,
-    title: "知る人ぞ知る焼酎！#{n}",
-    caption: "なんと焼き芋焼酎！焼酎好きの知人から進められたのですが香ばしくて絶品です！#{n}",
-    location: "溜池山王",
+  Post.create!(
+    user_id: 1,
+    title: "焼き芋焼酎#{n + 1}",
+    post_image: File.open("./app/assets/images/alcohol4.jpg"),
+    caption: "焼酎好きに教えてもらいましたー！香ばしくて美味しい！",
+    location: "渋谷",
   )
-  File.open("./app/assets/images/alcohol4.jpg") do |file|
-    post.image = file
-  end
-  post.post_image_id = post.image.id
-  post.save
+end
+
+3.times do |n|
+  Post.create!(
+    user_id: 2,
+    title: "Barで飲んでます#{n + 1}",
+    post_image: File.open("./app/assets/images/alcohol6.jpg"),
+    caption: "おいしい",
+    location: "新宿",
+  )
+end
+
+3.times do |n|
+  Post.create!(
+    user_id: 1,
+    title: "GODIVA！#{n + 1}",
+    post_image: File.open("./app/assets/images/alcohol1.jpg"),
+    caption: "GODIVAのリキュール飲んでみました！",
+    location: "渋谷",
+  )
+end
+
+4.times do |n|
+  Post.create!(
+    user_id: 2,
+    title: "魔王！！#{n + 1}",
+    post_image: File.open("./app/assets/images/alcohol2.jpg"),
+    caption: "最高級焼酎！美味い！！！",
+    location: "新宿",
+  )
+end
+
+3.times do |n|
+  Post.create!(
+    user_id: 3,
+    title: "十四代！！#{n + 1}",
+    post_image: File.open("./app/assets/images/alcohol3.jpg"),
+    caption: "1杯で1500円！",
+    location: "新宿",
+  )
+end
+
+2.times do |n|
+  Post.create!(
+    user_id: 1,
+    title: "やっぱりビール！！#{n + 1}",
+    post_image: File.open("./app/assets/images/alcohol5.jpg"),
+    caption: "仕事終わりキンキンに冷えたビール！最高です！！！",
+    location: "渋谷",
+  )
 end
