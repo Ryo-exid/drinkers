9b771f9e0df0f6a910167d6682bbf469b2e9af92
Ryo-exid <sakura4oneok@gmail.com>
2021-06-09 12:37:34 +0000

    [Add]いいね機能の非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,11 @@ class FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     @favorite = @post.favorites.create(user_id: current_user.id)
     @favorite.save
-    redirect_to request.referer
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @favorite = @post.favorites.find_by(post_id: params[:post_id], user_id: current_user.id)
     @favorite.destroy
-    redirect_to request.referer
   end
 end
