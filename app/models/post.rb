10d5b2964ca709673dab8095e47834ef1cb72b0b
Ryo-exid <sakura4oneok@gmail.com>
2021-06-09 08:31:30 +0000

    [Add]いいね機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   attachment :post_image
+  has_many :favorites, dependent: :destroy
+
+  def favorite_user(user_id)
+    favorites.find_by(user_id: user.id)
+  end#
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get "homes/about" => "homes#about"
   devise_for :users
   resources :users
-  resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
