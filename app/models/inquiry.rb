b462ab68dd6811ac71694bb98b9ccf3b204eba80
Ryo-exid <sakura4oneok@gmail.com>
2021-06-16 14:59:29 +0000

    [Fix]不要なコメントアウトを削除

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,6 +1,4 @@
 class Inquiry < ApplicationRecord
-  # attr_accessor :name, :email, :message
-
   validates :name, presence: true
   validates :email, presence: true
   validates :message, presence: true
