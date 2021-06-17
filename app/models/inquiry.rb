a61f59ec58d302fd34e127c8b90ab8958f657c58
Ryo-exid <sakura4oneok@gmail.com>
2021-06-17 09:00:01 +0000

    [Fix]お問い合わせ時のメールアドレスのバリデーション

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,5 +1,6 @@
 class Inquiry < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}}
   validates :message, presence: true
 end
