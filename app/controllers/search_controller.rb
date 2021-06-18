4cefb647833cbc7c4132ab3e93841eb2f298da32
Ryo-exid <sakura4oneok@gmail.com>
2021-06-18 14:13:38 +0000

    [Fix]検索窓のレイアウト調整

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
   def search
     @range = params[:range]
-    if @range == "投稿" || @range == "場所"
+    if @range == "タイトル" || @range == "場所"
       @posts = Post.looks(params[:search], params[:word]).page(params[:page]).reverse_order
     else
       @users = User.looks(params[:search], params[:word])
