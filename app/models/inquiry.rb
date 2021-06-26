f5094adc3e233c248a4c6a004b572b6587c0b2ab
Ryo-exid <sakura4oneok@gmail.com>
2021-06-26 11:22:25 +0000

    [Fix]nameの文字数上限を30文字以内に変更

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,5 +1,5 @@
 class Inquiry < ApplicationRecord
-  validates :name, { presence: { message: "を入力してください（20文字以内）" }, length: { maximum: 20 } }
+  validates :name, { presence: { message: "を入力してください（30文字以内）" }, length: { maximum: 30 } }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze # 正規表現
   validates :email, { presence: true, format: { with: VALID_EMAIL_REGEX } }
   validates :message, { presence: true, length: { maximum: 500 } }
--- spec/models/inquiry_spec.rb
+++ spec/models/inquiry_spec.rb
@@ -3,44 +3,44 @@ require 'rails_helper'
 RSpec.describe "Inquiryモデルのテスト", type: :model do
   describe "バリデーションのテスト" do
     subject { inquiry.valid? }
-    
+
     let!(:inquiry) { build(:inquiry) }
-    
+
     context "nameカラム" do
       it "空欄でないこと" do
         inquiry.name = ""
         is_expected.to eq false
       end
-      
-      it "20文字以下であること: 20文字は◯" do
-        inquiry.name = Faker::Lorem.characters(number: 20)
+
+      it "30文字以下であること: 30文字は◯" do
+        inquiry.name = Faker::Lorem.characters(number: 30)
         is_expected.to eq true
       end
-      
-      it "20文字以下であること: 21文字は×" do
-        inquiry.name = Faker::Lorem.characters(number: 21)
+
+      it "30文字以下であること: 31文字は×" do
+        inquiry.name = Faker::Lorem.characters(number: 31)
         is_expected.to eq false
       end
     end
-    
+
     context "emailカラム" do
       it "空欄でないこと" do
         inquiry.email = ""
         is_expected.to eq false
       end
     end
-    
+
     context "messageカラム" do
       it "空欄でないこと" do
         inquiry.message = ""
         is_expected.to eq false
       end
-      
+
       it "500文字以下であること: 20文字は◯" do
         inquiry.message = Faker::Lorem.characters(number: 500)
         is_expected.to eq true
       end
-      
+
       it "500文字以下であること: 501文字は×" do
         inquiry.message = Faker::Lorem.characters(number: 501)
         is_expected.to eq false
