c38821b4344c80513e5d9c4d316383089e8471fd
Ryo-exid <sakura4oneok@gmail.com>
2021-06-26 11:11:50 +0000

    [Fix]バリデーションに文字制限を追加

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -2,5 +2,5 @@ class Inquiry < ApplicationRecord
   validates :name, { presence: { message: "を入力してください（20文字以内）" }, length: { maximum: 20 } }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze # 正規表現
   validates :email, { presence: true, format: { with: VALID_EMAIL_REGEX } }
-  validates :message, { presence: true, length: { maximum: 300 } }
+  validates :message, { presence: true, length: { maximum: 500 } }
 end
