bbbe520137a2f60d31dc534bf889b96fbc209e7a
Ryo-exid <sakura4oneok@gmail.com>
2021-06-26 12:28:03 +0000

    [Add]Relationshipモデルのテスト

---
+++ spec/models/relationship_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Relationshipモデルのテスト", type: :model do
+
+  describe "アソシエーションのテスト" do
+    context "Userモデルとの関係" do
+      it "N:1となっている" do
+        expect(Relationship.reflect_on_association(:follower).macro).to eq :belongs_to
+        expect(Relationship.reflect_on_association(:followed).macro).to eq :belongs_to
+      end
+    end
+  end
+end
