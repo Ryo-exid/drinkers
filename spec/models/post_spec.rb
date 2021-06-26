c659a97d7c54cb67ac4aaff6e102fc04c747bc72
Ryo-exid <sakura4oneok@gmail.com>
2021-06-26 10:44:08 +0000

    [Fix]Postモデルのテスト

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -79,5 +79,23 @@ RSpec.describe "Postモデルのテスト", type: :model do
         expect(Post.reflect_on_association(:user).macro).to eq :belongs_to
       end
     end
+
+    context "Favoriteモデルとの関係" do
+      it "1:Nとなっている" do
+        expect(Post.reflect_on_association(:favorites).macro).to eq :has_many
+      end
+    end
+
+    context "PostCommentモデルとの関係" do
+      it "1:Nとなっている" do
+        expect(Post.reflect_on_association(:post_comments).macro).to eq :has_many
+      end
+    end
+
+    context "Notificationモデルとの関係" do
+      it "1:Nとなっている" do
+        expect(Post.reflect_on_association(:notifications).macro).to eq :has_many
+      end
+    end
   end
 end
