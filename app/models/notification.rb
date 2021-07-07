e4eae82353b4bdbc6f1585d34fe13daff2b1a6fa
Ryo-exid <sakura4oneok@gmail.com>
2021-07-07 09:54:43 +0000

    [Fix]インデント調整

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,6 @@
 class Notification < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
-
+  
   belongs_to :post, optional: true
   belongs_to :post_comment, optional: true
   belongs_to :chat, optional: true
