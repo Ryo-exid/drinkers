a1278ca4daa5211e645bcfca4015e50c635437c6
Ryo-exid <sakura4oneok@gmail.com>
2021-07-08 07:06:44 +0000

    [Fix]インデント調整

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,6 +1,6 @@
 class InquiryMailer < ApplicationMailer
   default from: '"Drinker × Drinker"<from@example.com>' # 送信元アドレス
-
+  
   def received_email(inquiry)
     @inquiry = inquiry
     mail bcc: ENV['APPLICATION_EMAIL'], subject: "お問い合わせ[新着]"
