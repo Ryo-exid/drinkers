5ee333c45e7baabe5fc43851746f882810b277e6
Ryo-exid <sakura4oneok@gmail.com>
2021-07-23 07:35:09 +0000

    [Fix]不要な記述を削除

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,7 +2,6 @@ module NotificationsHelper
   def notification_form(notification)
     @visiter = notification.visiter
     @post_comment = nil
-    your_item = link_to 'あなたの投稿', post_path(notification), style: "font-weight: bold;"
     @visiter_post_comment = notification.post_comment_id
     # notification.actionがfollowかfavoriteかpost_commentか
     case notification.action
